9262b4cb5d10ff50c70023c04e5a9048c3afb6e4
hulakantony <hulak.antony1992@gmail.com>
2016-08-04 19:18:18 +0300

    carousel

--- MY_FINAL_TASK/js/dropdown_menu.js
+++ MY_FINAL_TASK/js/dropdown_menu.js
@@ -1,5 +1,29 @@
-var menuButton = document.querySelector('.menu-phone a');
-var dropMenu = document.querySelector('.dropdown-menu-wrap');
-menuButton.onclick = function(e){
-	dropMenu.classList.toggle('pressed');
+
+function carouselMove(){
+	
+	
+
 }
+var carousel = document.querySelectorAll('.carousel');
+var points = document.querySelectorAll('.points li a');
+function addActiveClass(elem, shift){
+	for(var i =0; i< points.length; i++){
+		var point = points[i];
+		point.classList.remove('active');
+	}
+	points[1].classList.add('active');
+	carousel.style.left = -100 + '%';
+	return false;
+}
+points[1].addEventListener('click', addActiveClass);
+
+(function(){
+	var position = 0;
+	function nextStep(){
+		carousel.style.left = position - 100 + '%';
+		position = parseInt(carousel.getComputedStyle().left);
+		if(position === 300){
+			position = 0;
+		}
+	}
+})()
